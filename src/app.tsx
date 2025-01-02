64e1d5b0aa677e50eb894681c9af18f1c85bdf95
cp-20 <n.naoki.1587@gmail.com>
2025-01-02 20:42:59 +0900

    🧄 🍎 🥯 いらないものを消した

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,13 @@
-import "@unocss/reset/tailwind.css";
-import "virtual:uno.css";
+import '@unocss/reset/tailwind.css';
+import 'virtual:uno.css';
 
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
+import { Router } from '@solidjs/router';
+import { FileRoutes } from '@solidjs/start/router';
+import { Suspense } from 'solid-js';
 
 export default function App() {
   return (
-    <Router
-      root={(props) => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
+    <Router root={(props) => <Suspense>{props.children}</Suspense>}>
       <FileRoutes />
     </Router>
   );
