70b1d1ea1b978bf66073ce604e4fcf0c3c9c056e
cp-20 <n.naoki.1587@gmail.com>
2025-03-26 20:34:35 +0900

    update

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+    rollupConfig: {
+      external: ["node:async_hooks"],
+    },
+  },
   vite: {
     plugins: [UnoCSS()],
   },
