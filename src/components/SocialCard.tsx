177310bc7a83f44a10881b8058a32743ad639433
cp-20 <n.naoki.1587@gmail.com>
2025-03-30 17:24:07 +0900

    Social Card

--- src/components/SocialCard.tsx
+++ src/components/SocialCard.tsx
@@ -1,13 +1,13 @@
-import clsx from 'clsx';
-import type { Component, ComponentProps, JSX } from 'solid-js';
+import clsx from "clsx";
+import type { Component, ComponentProps, JSX } from "solid-js";
 
 type SocialCardProps = {
   mediaLabel: string;
-  idLabel: string;
+  idLabel?: string;
   icon: JSX.Element;
 };
 
-export const SocialCard: Component<ComponentProps<'a'> & SocialCardProps> = ({
+export const SocialCard: Component<ComponentProps<"a"> & SocialCardProps> = ({
   mediaLabel,
   idLabel,
   icon,
@@ -17,12 +17,12 @@ export const SocialCard: Component<ComponentProps<'a'> & SocialCardProps> = ({
     {...props}
     class={clsx(
       props.class,
-      'flex justify-between items-center px-4 py-2 rounded font-bold'
+      "flex justify-between items-center px-4 py-2 rounded font-bold",
     )}
   >
     <div class="flex flex-col">
       <span class="text-sm">{mediaLabel}</span>
-      <span class="-mt-1">@{idLabel}</span>
+      {idLabel && <span class="-mt-1">@{idLabel}</span>}
     </div>
     {icon}
   </a>
--- src/components/models/home/ProfileCard.tsx
+++ src/components/models/home/ProfileCard.tsx
@@ -1,9 +1,12 @@
-import type { Component } from 'solid-js';
-import avatar from '~/assets/avatar.png';
-import { Card } from '~/components/Card';
-import { Mixi2Icon } from '~/components/icons/mixi2';
-import { Link } from '~/components/Link';
-import { SocialCard } from '~/components/SocialCard';
+import type { Component } from "solid-js";
+import avatar from "~/assets/avatar.png";
+import { Card } from "~/components/Card";
+import { DiscordIcon } from "~/components/icons/discord";
+import { Mixi2Icon } from "~/components/icons/mixi2";
+import { QiitaIcon } from "~/components/icons/qiita";
+import { ZennIcon } from "~/components/icons/zenn";
+import { Link } from "~/components/Link";
+import { SocialCard } from "~/components/SocialCard";
 
 export const ProfileCard: Component = () => (
   <Card class="grid p-8 grid-cols-1 md:grid-cols-2 gap-8">
@@ -20,7 +23,7 @@ export const ProfileCard: Component = () => (
           mediaLabel="Twitter"
           idLabel="__cp20__"
           icon={<div class="i-mdi-twitter text-[#1da1f2] size-8 shrink-0" />}
-          class="bg-[#1da1f2]/20 hover:bg-[#1da1f2]/30 text-gray-2"
+          class="bg-[#1da1f2]/20 hover:bg-[#1da1f2]/30 text-gray-2 transition-colors duration-100"
         />
         <SocialCard
           href="https://mixi.social/@cp20"
@@ -36,6 +39,26 @@ export const ProfileCard: Component = () => (
           icon={<div class="i-mdi-github text-white size-8 shrink-0" />}
           class="bg-black/30 hover:bg-black/50 text-gray-2 transition-colors duration-100"
         />
+        <div class="flex gap-2">
+          <SocialCard
+            href="https://discord.gg/YQ7negGTUK"
+            mediaLabel="Discord"
+            icon={<DiscordIcon width={24} height={24} class="text-white" />}
+            class="bg-[#5865f2]/30 hover:bg-[#5865f2]/40 text-gray-2 flex-1 transition-colors duration-100"
+          />
+          <SocialCard
+            href="https://zenn.dev/cp20"
+            mediaLabel="Zenn"
+            icon={<ZennIcon width={24} height={24} class="text-[#3ea8ff]" />}
+            class="bg-[#3ea8ff]/20 hover:bg-[#3ea8ff]/30 text-gray-2 flex-1 transition-colors duration-100"
+          />
+          <SocialCard
+            href="https://qiita.com/cp20"
+            mediaLabel="Qiita"
+            icon={<QiitaIcon width={24} height={24} class="text-[#55c500]" />}
+            class="bg-[#55c500]/20 hover:bg-[#55c500]/30 text-gray-2 flex-1 transition-colors duration-100"
+          />
+        </div>
       </div>
     </div>
     <div class="flex flex-col gap-2 justify-center items-center">
