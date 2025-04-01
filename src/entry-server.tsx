7745f9f5b83db33683295d626d4252fa5e16577e
cp-20 <n.naoki.1587@gmail.com>
2025-04-01 22:12:16 +0900

    fix favicon

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,7 +8,11 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" sizes="32x32" />
+          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.webmanifest" />
+
           <link rel="stylesheet" href="/app.css" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
