51af3b9ab9dc838ee4b95fc26d1ff9de3ca9771a
cp-20 <n.naoki.1587@gmail.com>
2025-04-04 16:35:52 +0900

    refactor

---
+++ src/personal-data/featured-series.ts
@@ -0,0 +1,38 @@
+export const featuredSeries = [
+  {
+    id: "houkago-kitaku-biyori",
+    title: "放課後帰宅びより",
+    author: "松田舞",
+    link: "https://comic-action.com/episode/4856001361584756651",
+  },
+  {
+    id: "kage-no-jitsuryokusha-ni-naritakute",
+    title: "陰の実力者になりたくて！",
+    author: "逢沢大介",
+    link: "https://comic-walker.com/detail/KC_003912_S",
+  },
+  {
+    id: "j-m",
+    title: "Ｊ⇔Ｍジェイエム",
+    author: "大武政夫",
+    link: "https://comic-walker.com/detail/KC_000789_S",
+  },
+  {
+    id: "koi-bumi-to-13sai-no-actress",
+    title: "恋文と13歳の女優（アクトレス）",
+    author: "じゃが",
+    link: "https://comic-fuz.com/manga/3149",
+  },
+  {
+    id: "bad-girl",
+    title: "ばっどがーる",
+    author: "肉丸",
+    link: "https://comic-fuz.com/manga/2664",
+  },
+  {
+    id: "mattaku-saikin-no-tantei-to-kitara",
+    title: "まったく最近の探偵ときたら",
+    author: "五十嵐正邦",
+    link: "https://comic-walker.com/detail/KC_001464_S",
+  },
+];
